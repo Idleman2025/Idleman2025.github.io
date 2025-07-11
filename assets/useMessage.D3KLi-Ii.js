07ab83f209ff6f6dda107246ceadb302a64ec9dd
teriri <eoezora@qq.com>
2025-07-11 18:00:45 +0800

    oops

--- assets/useMessage.D3KLi-Ii.js
+++
@@ -1,2 +0,0 @@
-import{aX as o}from"./index-DFl-DtYN.js";const n=(n,t="none")=>{o({title:n,icon:t})};export{n as u};
-//# sourceMappingURL=useMessage.D3KLi-Ii.js.map
---
+++ assets/useParent.zc7AofC8.js
@@ -0,0 +1,2 @@
+import{aN as n,L as r,aY as a,q as e,r as i}from"./index-CaBmDrrg.js";function t(t){const s=n(t,null);if(s){const n=r(),{link:i,unlink:t,internalChildren:l}=s;i(n),a(()=>t(n));return{parent:s,index:e(()=>l.indexOf(n))}}return{parent:null,index:i(-1)}}export{t as u};
+//# sourceMappingURL=useParent.zc7AofC8.js.map
